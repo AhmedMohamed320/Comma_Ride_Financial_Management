7cf06cb1b600f05922ea06f49042520cfcd84f2c
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-11-01 14:59:48 +0200

    edit

--- app/(mian)/Expenses/page.jsx
+++ app/(mian)/Expenses/page.jsx
@@ -6,6 +6,9 @@ import Nav from "@/components/Expenses/Nav";
 const page = () => {
     return (
         <section className={`mainContainer py-4 `}>
+            <p className="text-4xl font-semibold pb-4">
+            المصروفات
+          </p>
             <Nav />
             <Main />
         </section>
