e9666d88835ad451d3f117a7c2cb9063b53dcd4c
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-11-04 17:05:11 +0200

    edit

--- app/(mian)/Assets/page.jsx
+++ app/(mian)/Assets/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import classes from "./page.module.css";
 import { BiSolidEditAlt } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
